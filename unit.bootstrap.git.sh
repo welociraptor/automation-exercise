7b1d68a2c8dc15a8beebff4b523d1b18d264e4d9
markku.halinen <markku.halinen@sok.fi>
2025-03-17 10:55:41 +0200

    Add script to bootstrap Unit from git repository

---
+++ unit.bootstrap.git.sh
@@ -0,0 +1,21 @@
+#!/bin/sh -x
+
+# This script builds Unit from source code, using version specified in environment variable VERSION.
+
+# Install dependencies
+yum install -y git gcc which pcre2-devel
+
+# Clone desired version from repository
+git clone -b $VERSION https://github.com/nginx/unit.git
+
+# Build and install
+cd unit
+./configure
+make
+make install
+
+# Clean up
+cd ..
+rm -rf unit
+
+unitd --version
