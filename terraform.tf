5bed525e12604e2aad45cd11bc4f1757374d1ea3
welociraptor <17182067+welociraptor@users.noreply.github.com>
2025-03-17 22:27:59 +0200

    Add ssh key generation

--- terraform.tf
+++ terraform.tf
@@ -5,8 +5,14 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 3.76"
     }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "4.0.6"
+    }
   }
 }
+
+provider "tls" {}
 
 provider "aws" {
   region = "us-east-1"
@@ -51,6 +57,42 @@ data "aws_ami" "amazon_linux_2023" {
   }
 }
 
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = aws_vpc.main.id
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "tls_private_key" "ssh" {
+  algorithm = "ED25519"
+}
+
+resource "aws_key_pair" "ssh" {
+  key_name   = "ssh"
+  public_key = tls_private_key.ssh.public_key_openssh
+}
+
+resource "local_sensitive_file" "ssh_private_key" {
+  content         = tls_private_key.ssh.private_key_pem
+  filename        = "ssh.pem"
+  file_permission = "0600"
+}
+
+resource "aws_instance" "test" {
+  ami                    = "ami-08b5b3a93ed654d19"
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.public.id
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+  key_name               = aws_key_pair.ssh.key_name
+}
+
 output "vpc_id" {
   value = aws_vpc.main.id
 }
@@ -61,4 +103,9 @@ output "subnet_id" {
 
 output "source_ami" {
   value = data.aws_ami.amazon_linux_2023.id
+}
+
+output "ssh_key" {
+  sensitive = true
+  value     = tls_private_key.ssh.private_key_pem
 }
